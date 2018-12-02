2182eab8126c353b7c1048d81b2a490e109a703c
Kai Zhe <yewkaizhe@hotmail.com>
2018-12-02 23:05:49 +0800

    Updates

---
+++ precache-manifest.cfe980c51c5694d38ed680167c34bfc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b2175ab01fa3df0f711",
+    "url": "/NUSMods-Planner/static/js/runtime~main.6b2175ab.js"
+  },
+  {
+    "revision": "7811b05ef182fb68ac70",
+    "url": "/NUSMods-Planner/static/js/main.7811b05e.chunk.js"
+  },
+  {
+    "revision": "a9a075c8ddb70873378b",
+    "url": "/NUSMods-Planner/static/js/1.a9a075c8.chunk.js"
+  },
+  {
+    "revision": "7811b05ef182fb68ac70",
+    "url": "/NUSMods-Planner/static/css/main.8a6392e0.chunk.css"
+  },
+  {
+    "revision": "bf51431483a3aa78a87830a2c9d3d62d",
+    "url": "/NUSMods-Planner/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nusmods-planner/precache-manifest.72c18fc36b8fab4fc8473dafacab58ca.js"
+  "/NUSMods-Planner/precache-manifest.cfe980c51c5694d38ed680167c34bfc6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/nusmods-planner/index.html", {
+workbox.routing.registerNavigationRoute("/NUSMods-Planner/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
