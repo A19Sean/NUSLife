0b2b8c2dff847fd67650b96dfd1438daa0da2dd2
Kai Zhe <yewkaizhe@hotmail.com>
2018-12-02 19:20:47 +0800

    Updates

---
+++ precache-manifest.4293c84e5e3b13d4a0c76211e67b21c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nusmods-planner/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9c21c06ad19f146e727d",
+    "url": "/nusmods-planner/static/js/runtime~main.9c21c06a.js"
+  },
+  {
+    "revision": "6fe1705232c31945bf71",
+    "url": "/nusmods-planner/static/js/main.6fe17052.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/nusmods-planner/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "6fe1705232c31945bf71",
+    "url": "/nusmods-planner/static/css/main.b2001008.chunk.css"
+  },
+  {
+    "revision": "2de56cd2af640b073dfb063ae0af657a",
+    "url": "/nusmods-planner/index.html"
+  }
+];
