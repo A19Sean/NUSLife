aaefe7d7cf79d1036fd0c9f18780db1218e22715
A19Sean <sean.auyok@gmail.com>
2018-12-25 23:06:09 +0100

    Updates

---
+++ precache-manifest.bc8c8ba889e7b318cfddf27cc32cd77e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b10db1963b8e95497bb",
+    "url": "/NUSLife/static/js/runtime~main.7b10db19.js"
+  },
+  {
+    "revision": "b011979c05b38f702ea2",
+    "url": "/NUSLife/static/js/main.b011979c.chunk.js"
+  },
+  {
+    "revision": "09478223851f8734c54a",
+    "url": "/NUSLife/static/js/1.09478223.chunk.js"
+  },
+  {
+    "revision": "b011979c05b38f702ea2",
+    "url": "/NUSLife/static/css/main.f3adfcc3.chunk.css"
+  },
+  {
+    "revision": "9204f165bc1e4d3f4bf0853fd8d35579",
+    "url": "/NUSLife/index.html"
+  }
+];
