efca2e3888ca7df7aa516502784f0e1599a4817f
A19Sean <sean.auyok@gmail.com>
2019-01-02 03:01:42 +0800

    Updates

--- precache-manifest.cf6f60be6ff99635bc3808c125347c7f.js
+++ precache-manifest.cf6f60be6ff99635bc3808c125347c7f.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/NUSLife/static/js/runtime~main.7b10db19.js"
   },
   {
-    "revision": "b011979c05b38f702ea2",
-    "url": "/NUSLife/static/js/main.b011979c.chunk.js"
+    "revision": "b9a3193c0ffc0f372f4c",
+    "url": "/NUSLife/static/js/main.b9a3193c.chunk.js"
   },
   {
     "revision": "09478223851f8734c54a",
     "url": "/NUSLife/static/js/1.09478223.chunk.js"
   },
   {
-    "revision": "b011979c05b38f702ea2",
+    "revision": "b9a3193c0ffc0f372f4c",
     "url": "/NUSLife/static/css/main.f3adfcc3.chunk.css"
   },
   {
-    "revision": "9204f165bc1e4d3f4bf0853fd8d35579",
+    "revision": "ad18eefbbaff6e6a8e8808e29dd1cbd8",
     "url": "/NUSLife/index.html"
   }
 ];
